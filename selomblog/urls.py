c11199d75e2ec9d61552ac8a9ab1c4c51af546db
AITI Student <aiti-student@AITI-83.(none)>
2012-07-05 15:09:15 +0000

    working now

--- selomblog/urls.py
+++ selomblog/urls.py
@@ -7,11 +7,11 @@ admin.autodiscover()
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'selomblog.views.home', name='home'),
-    url(r'^selomblog/', include('selomblog.urls')),
+    #url(r'^selomblog/', include('selomblog.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
 )
