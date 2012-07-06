7517fb9fb5b20f468f06f05e86a7beb37800dc5d
AITI Student <aiti-student@AITI-83.(none)>
2012-07-06 17:08:05 +0000

    not done yet

--- selomblog/urls.py
+++ selomblog/urls.py
@@ -8,7 +8,7 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'selomblog.views.home', name='home'),
     #url(r'^selomblog/', include('selomblog.urls')),
-
+    url(r'^blog',include('blog.urls')),
     # Uncomment the admin/doc line below to enable admin documentation:
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
